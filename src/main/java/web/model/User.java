2d86e93e1df1643f0a9fd688436ed622a135ac2e
schlbq <vano.lem72@gmail.com>
2023-11-16 16:57:31 +0500

    update 4. Убрал аннотации и сделал private поле

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -9,16 +9,12 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "lastName")
     private String lastName;
 
-    @Column(name = "age")
     private int age;
 
     public User(String name, String lastName, int age) {
